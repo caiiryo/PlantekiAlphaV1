5697e927e6ef656aaa18f042f2c2c89c9c18fdce
cairyoo <wayneedelmundo@gmail.com>
2025-02-19 02:09:48 +0800

    alpha beta

---
+++ planteki.py
@@ -0,0 +1,22 @@
+from kivy.core.text import LabelBase
+from kivy.uix.screenmanager import ScreenManager
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.core.window import Window
+Window.size = (440, 956)
+
+
+class Planteki(MDApp):
+
+    def build(self):
+        screen_manager = ScreenManager()
+        screen_manager.add_widget(Builder.load_file("main.kv"))
+        #screen_manager.add_widget(Builder.load_file("login.kv"))
+        #screen_manager.add_widget(Builder.load_file("signup.kv"))
+        return screen_manager
+
+if __name__ == "__main__":
+    LabelBase.register(name="SFDispHeavy", fn_regular="D:\Planteki\Fonts\SFProDisplay\SFProText-Heavy.ttf")
+    LabelBase.register(name="SFTextBold", fn_regular="D:\Planteki\Fonts\SFProDisplay\SFProText-Bold.ttf")
+
+    Planteki().run()
