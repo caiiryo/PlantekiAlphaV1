559ea520c5e35b609151309ef60e4a457f3b6ae4
Gellena <105719256+Pisbol@users.noreply.github.com>
2025-02-19 19:18:04 +0800

    Update planteki.py

--- planteki.py
+++ planteki.py
@@ -5,6 +5,8 @@ from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.graphics import Color, Rectangle
+from kivymd.uix.button import MDIconButton
+
 Window.size = (440, 856)
 
 
@@ -16,6 +18,7 @@ class Planteki(MDApp):
         screen_manager.add_widget(Builder.load_file("landing.kv"))
         screen_manager.add_widget(Builder.load_file("login.kv"))
         screen_manager.add_widget(Builder.load_file("signup.kv"))
+        screen_manager.add_widget(Builder.load_file("dashboard.kv"))
         return screen_manager
 
 if __name__ == "__main__":
